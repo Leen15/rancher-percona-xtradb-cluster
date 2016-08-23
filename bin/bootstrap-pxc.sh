d5dca4d5751631121051a252124afb2f58ebb11f
Luca <leen15@msn.com>
2016-08-23 22:59:16 +0200

    set password in mysql5.7 is deprecated.

--- bin/bootstrap-pxc.sh
+++ bin/bootstrap-pxc.sh
@@ -10,7 +10,7 @@ echo "=> Bootstrapping PXC cluster"
 mysql_install_db --datadir=${PXC_VOLUME} >/dev/null 2>&1
 echo "CREATE USER 'root'@'%' IDENTIFIED BY '${PXC_ROOT_PASSWORD}';" > /tmp/init.sql
 echo "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> /tmp/init.sql
-echo "UPDATE mysql.user set Password=PASSWORD('${PXC_ROOT_PASSWORD}') where user='root';" >> /tmp/init.sql
+echo "ALTER USER root IDENTIFIED BY '${PXC_ROOT_PASSWORD}';" >> /tmp/init.sql
 echo "DELETE FROM mysql.user WHERE User='';" >> /tmp/init.sql
 echo "DROP DATABASE test;" >> /tmp/init.sql
 echo "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';" >> /tmp/init.sql
@@ -33,6 +33,3 @@ fi
 
 echo "=> Starting PXC Cluster"
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord_bootstrap.conf
-
-# Start the first init script because without this other nodes cannot join cluster
- mysql -u root -pPXC_ROOT_PASSWORD < /tmp/init.sql
