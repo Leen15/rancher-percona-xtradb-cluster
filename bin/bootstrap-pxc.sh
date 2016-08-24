d80d9a03999d947dde9c46f20972b60d7ecc1b0a
Luca <leen15@msn.com>
2016-08-24 08:50:05 +0200

    Update bootstrap-pxc.sh

--- bin/bootstrap-pxc.sh
+++ bin/bootstrap-pxc.sh
@@ -12,8 +12,6 @@ echo "CREATE USER 'root'@'%' IDENTIFIED BY '${PXC_ROOT_PASSWORD}';" > /tmp/init.
 echo "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> /tmp/init.sql
 echo "ALTER USER root IDENTIFIED BY '${PXC_ROOT_PASSWORD}';" >> /tmp/init.sql
 echo "DELETE FROM mysql.user WHERE User='';" >> /tmp/init.sql
-echo "DROP DATABASE test;" >> /tmp/init.sql
-echo "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';" >> /tmp/init.sql
 echo "CREATE USER 'sstuser'@'%' IDENTIFIED BY '${PXC_SST_PASSWORD}';" >> /tmp/init.sql
 echo "GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'sstuser'@'%';" >> /tmp/init.sql
 echo "GRANT PROCESS ON *.* TO 'clustercheckuser'@'localhost' IDENTIFIED BY 'clustercheckpassword!';" >> /tmp/init.sql
